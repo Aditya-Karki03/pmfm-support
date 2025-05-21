b953ac27f36f246e732605f0caa95621e311df86
Aditya Karki <adityakarki201@gmail.com>
2025-05-21 22:41:42 +0530

    UI for the support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex flex-col items-center bg-gray-200 px-10 border border-red-700 justify-between py-4">
+      <div className=" w-full h-20 flex justify-center items-center">
+        <h1 className="font-bold text-lg sm:text-2xl md:text-4xl">
+          PMFM Support
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className=""></div>
+      <div className="min-h-52 border border-black/10 justify-center px-2 md:px-4 flex flex-col gap-3 w-full rounded-lg shadow-2xl ">
+        <input
+          type="text"
+          placeholder="Type your question"
+          className="border border-black/30 p-2 md:p-3 rounded-md outline-none"
+        />
+        <button className="bg-black text-white py-2 rounded-md">Ask</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    </div>
+  );
+};
+export default App;
